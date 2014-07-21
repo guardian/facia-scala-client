926fc9d8c988c067c54675da1e3e92dd61744883
Robert Berry <robert.berry@theguardian.com>
2014-07-21 13:22:36 +0100

    Add strict types for the item metadata so as to accommodate supporting content

--- src/main/scala/com/gu/facia/client/models/Collection.scala
+++ src/main/scala/com/gu/facia/client/models/Collection.scala
@@ -3,6 +3,36 @@ package com.gu.facia.client.models
 import org.joda.time.DateTime
 import play.api.libs.json.Json
 
+object SupportingItemMetaData {
+  implicit val jsonReads = Json.reads[SupportingItemMetaData]
+}
+
+case class SupportingItemMetaData(
+  headline: Option[String],
+  imageAdjust: Option[String],
+  group: Option[String]
+)
+
+object SupportingItem {
+  implicit val jsonReads = Json.reads[SupportingItem]
+}
+
+case class SupportingItem(
+  id: String,
+  meta: Option[SupportingItemMetaData]
+)
+
+object TrailMetaData {
+  implicit val jsonReads = Json.reads[TrailMetaData]
+}
+
+case class TrailMetaData(
+  headline: Option[String],
+  imageAdjust: Option[String],
+  group: Option[String],
+  supporting: Option[List[SupportingItem]]
+)
+
 object Trail {
   implicit val jsonReads = Json.reads[Trail]
 }
@@ -10,7 +40,7 @@ object Trail {
 case class Trail(
   id: String,
   frontPublicationDate: Long,
-  meta: Map[String, String]
+  meta: TrailMetaData
 )
 
 object Collection {
