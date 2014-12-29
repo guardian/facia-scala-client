a819198971f558d3ee0a6dde0860d7d51ba7925f
Adam Fisher <adam.fisher@guardian.co.uk>
2014-12-29 16:52:15 +0000

    Default keys for integration config are now obviously invalid

--- fapi-client/src/test/scala/lib/IntegrationTestConfig.scala
+++ fapi-client/src/test/scala/lib/IntegrationTestConfig.scala
@@ -6,8 +6,8 @@ import com.gu.contentapi.client.GuardianContentClient
 import com.gu.facia.client.{AmazonSdkS3Client, ApiClient}
 
 trait IntegrationTestConfig extends ExecutionContext {
-  implicit val capiClient = new GuardianContentClient(apiKey = "test")
+  implicit val capiClient = new GuardianContentClient(apiKey = "API_KEY")
 
-  private val amazonS3Client = new AmazonS3Client(new BasicAWSCredentials("key", "secret-key"))
+  private val amazonS3Client = new AmazonS3Client(new BasicAWSCredentials("ACCESS_KEY", "SECRET_KEY"))
   implicit val apiClient: ApiClient = ApiClient("aws-frontend-store", "DEV", AmazonSdkS3Client(amazonS3Client))
 }
