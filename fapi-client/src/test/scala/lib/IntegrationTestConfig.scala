8b7ce0feb618e40ae064e41f1525d7cf04cec08a
Robert Berry <robert.berry@theguardian.com>
2015-01-02 15:09:04 +0000

    Use default provider chain in integration tests

--- fapi-client/src/test/scala/lib/IntegrationTestConfig.scala
+++ fapi-client/src/test/scala/lib/IntegrationTestConfig.scala
@@ -1,6 +1,5 @@
 package lib
 
-import com.amazonaws.auth.BasicAWSCredentials
 import com.amazonaws.services.s3.AmazonS3Client
 import com.gu.contentapi.client.GuardianContentClient
 import com.gu.facia.client.{AmazonSdkS3Client, ApiClient}
@@ -8,6 +7,6 @@ import com.gu.facia.client.{AmazonSdkS3Client, ApiClient}
 trait IntegrationTestConfig extends ExecutionContext {
   implicit val capiClient = new GuardianContentClient(apiKey = "API_KEY")
 
-  private val amazonS3Client = new AmazonS3Client(new BasicAWSCredentials("ACCESS_KEY", "SECRET_KEY"))
+  private val amazonS3Client = new AmazonS3Client()
   implicit val apiClient: ApiClient = ApiClient("aws-frontend-store", "DEV", AmazonSdkS3Client(amazonS3Client))
 }
