8daf979bb5240f18750702f4a92f67955ad45235
Robert Berry <robert.berry@theguardian.com>
2015-01-02 15:10:36 +0000

    Get CAPI key from environment var

--- fapi-client/src/test/scala/lib/IntegrationTestConfig.scala
+++ fapi-client/src/test/scala/lib/IntegrationTestConfig.scala
@@ -5,7 +5,7 @@ import com.gu.contentapi.client.GuardianContentClient
 import com.gu.facia.client.{AmazonSdkS3Client, ApiClient}
 
 trait IntegrationTestConfig extends ExecutionContext {
-  implicit val capiClient = new GuardianContentClient(apiKey = "API_KEY")
+  implicit val capiClient = new GuardianContentClient(apiKey = scala.sys.env.getOrElse("CONTENT_API_KEY", ""))
 
   private val amazonS3Client = new AmazonS3Client()
   implicit val apiClient: ApiClient = ApiClient("aws-frontend-store", "DEV", AmazonSdkS3Client(amazonS3Client))
