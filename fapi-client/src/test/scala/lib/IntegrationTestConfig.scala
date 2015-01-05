6c50010d66c32b6decb8ed93f420ed4720887bd2
Francis Carr <francis.carr@guardian.co.uk>
2015-01-05 14:37:31 +0000

    Add TestContentApiClient to allow for overriding of targetUrl

--- fapi-client/src/test/scala/lib/IntegrationTestConfig.scala
+++ fapi-client/src/test/scala/lib/IntegrationTestConfig.scala
@@ -4,8 +4,17 @@ import com.amazonaws.services.s3.AmazonS3Client
 import com.gu.contentapi.client.GuardianContentClient
 import com.gu.facia.client.{AmazonSdkS3Client, ApiClient}
 
+private case class TestContentApiClient(override val apiKey: String, override val targetUrl: String) extends GuardianContentClient(apiKey)
+
 trait IntegrationTestConfig extends ExecutionContext {
-  implicit val capiClient = new GuardianContentClient(apiKey = scala.sys.env.getOrElse("CONTENT_API_KEY", ""))
+  val apiKey: String = scala.sys.env.getOrElse("CONTENT_API_KEY", "")
+  val targetUrl: Option[String] = scala.sys.env.get("FACIA_CLIENT_TARGET_URL")
+
+  implicit val capiClient: GuardianContentClient =
+    targetUrl.fold(ifEmpty = new GuardianContentClient(apiKey)){ targetUrl =>
+      new TestContentApiClient(
+        apiKey,
+        targetUrl)}
 
   private val amazonS3Client = new AmazonS3Client()
   implicit val apiClient: ApiClient = ApiClient("aws-frontend-store", "DEV", AmazonSdkS3Client(amazonS3Client))
