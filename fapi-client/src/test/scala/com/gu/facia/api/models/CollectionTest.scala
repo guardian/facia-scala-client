457ae18176bfb230e74ddeb1b3526fb680edac75
Francis Carr <francis.carr@guardian.co.uk>
2015-01-06 15:12:37 +0000

    Add tests for liveIdsWithoutSnaps

--- fapi-client/src/test/scala/com/gu/facia/api/models/CollectionTest.scala
+++ fapi-client/src/test/scala/com/gu/facia/api/models/CollectionTest.scala
@@ -85,4 +85,35 @@ class CollectionTest extends FreeSpec with ShouldMatchers with MockitoSugar with
       curatedContent.map(_.content.id) should equal(List("content-id"))
     }
   }
+
+  "liveIdsWithoutSnaps" - {
+    val trailOne = Trail("internal-code/content/1", 1, Some(trailMetadata))
+    val trailTwo = Trail("internal-code/content/2", 1, Some(trailMetadata))
+    val trailThree = Trail("artanddesign/gallery/2014/feb/28/beyond-basquiat-black-artists", 1, Some(trailMetadata))
+    val snapOne = Trail("snap/1415985080061", 1, Some(trailMetadata))
+    val snapTwo = Trail("snap/5345345215342", 1, Some(trailMetadata))
+
+    val collectionJsonTwo = collectionJson.copy(live = List(trailOne, snapOne, snapTwo, trailTwo, trailThree))
+
+    val collection = Collection.fromCollectionJsonConfigAndContent("id", Some(collectionJsonTwo), collectionConfig)
+
+    "Collection should start with 5 items" in {
+      collection.live.length should be (5)
+    }
+
+    "Removes snaps from a collection and returns the ids to query" in {
+      Collection.liveIdsWithoutSnaps(collection) should be
+        List("internal-code/content/1",
+          "internal-code/content/2",
+          "artanddesign/gallery/2014/feb/28/beyond-basquiat-black-artists")
+    }
+
+    "Empty out snaps" in {
+      val collectionJsonAllSnaps = collectionJson.copy(live = List(snapOne, snapTwo))
+      val collectionAllSnaps = Collection.fromCollectionJsonConfigAndContent("id", Some(collectionJsonAllSnaps), collectionConfig)
+
+      Collection.liveIdsWithoutSnaps(collectionAllSnaps) should be (List.empty)
+    }
+
+  }
 }
