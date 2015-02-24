1a81eb266cc2561966a626d7ba3153192c880c7a
Andrew Bulhak <andrew.bulhak@theguardian.com>
2015-02-24 11:12:56 +0000

    Added tests for kickerText

--- fapi-client/src/test/scala/com/gu/facia/api/utils/ItemKickerTest.scala
+++ fapi-client/src/test/scala/com/gu/facia/api/utils/ItemKickerTest.scala
@@ -32,4 +32,35 @@ class ItemKickerTest extends FreeSpec with ShouldMatchers with MockitoSugar with
       ItemKicker.fromContentAndTrail(content, trailMetadata, Some(collectionConfig)).value shouldBe a [SectionKicker]
     }
   }
+
+  "kickerText" - {
+    "should return a textual description for Breaking News kickers" in {
+      ItemKicker.kickerText(BreakingNewsKicker) shouldBe Some("Breaking")
+    }
+
+    "should return a textual description for Analysis kickers" in {
+      ItemKicker.kickerText(AnalysisKicker) shouldBe Some("Analysis")
+    }
+
+    "should return a textual description for Review kickers" in {
+      ItemKicker.kickerText(ReviewKicker) shouldBe Some("Review")
+    }
+
+    "should return a textual description for Cartoon kickers" in {
+      ItemKicker.kickerText(CartoonKicker) shouldBe Some("Cartoon")
+    }
+
+    "should return a textual description for tag kickers" in {
+      ItemKicker.kickerText(TagKicker("Aberdeen-Grampian", "", "aberdeen-grampian/aberdeen-grampian")) shouldBe Some("Aberdeen-Grampian")
+    }
+
+    "should return a textual description for section kickers" in {
+      ItemKicker.kickerText(SectionKicker("Football", "")) shouldBe Some("Football")
+    }
+
+    "should return nothing for free HTML kickers" in {
+      ItemKicker.kickerText(FreeHtmlKicker("<b>Something</b>")) shouldBe None
+      ItemKicker.kickerText(FreeHtmlKickerWithLink("<b>Something</b>", "http://www.theguardian.com/football")) shouldBe None
+    }
+  }
 }
