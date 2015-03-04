8c3980a99b84f5902888fc056f08005be189b82f
Francis Carr <francis.carr@guardian.co.uk>
2015-03-04 17:09:16 +0000

    Add tests for itemKicker for CuratedContent and SupportingCuratedContent

--- fapi-client/src/test/scala/com/gu/facia/api/models/CuratedContentTest.scala
+++ fapi-client/src/test/scala/com/gu/facia/api/models/CuratedContentTest.scala
@@ -1,9 +1,11 @@
 package com.gu.facia.api.models
 
-import com.gu.contentapi.client.model.Content
+import com.gu.contentapi.client.model.{Tag, Content}
+import com.gu.facia.api.utils.{TagKicker, SectionKicker}
 import com.gu.facia.client.models.{CollectionConfigJson, TrailMetaData}
 import org.scalatest.{FreeSpec, Matchers}
-import play.api.libs.json.JsString
+import play.api.libs.json.{JsBoolean, JsString}
+import org.scalatest.OptionValues._
 
 class CuratedContentTest extends FreeSpec with Matchers {
 
@@ -35,6 +37,83 @@ class CuratedContentTest extends FreeSpec with Matchers {
     "should resolve the headline from Content webTitle" in {
       val curatedContent = CuratedContent.fromTrailAndContent(contentWithoutFieldHeadline, trailMetaDataWithoutHeadline, collectionConfig)
       curatedContent.headline should be ("contentWithoutFieldHeadlineHeadline")
+    }
+  }
+
+  "CuratedContent itemKicker" - {
+    val emptyContent = Content(
+      "content-id", Some("section"), Some("Section Name"), None, "webTitle", "webUrl", "apiUrl",
+      fields = Some(Map("internalContentCode" -> "CODE", "headline" -> "Content headline", "href" -> "Content href", "trailText" -> "Content trailtext", "byline" -> "Content byline")),
+      List(Tag("id", "type", None, None, "", "", "")), None, Nil, None)
+    val emptyTrailMetaData = TrailMetaData(Map.empty)
+    val collectionConfig = CollectionConfig.fromCollectionJson(CollectionConfigJson.withDefaults())
+    val collectionConfigShowSections = CollectionConfig.fromCollectionJson(CollectionConfigJson.withDefaults(showSections = Option(true)))
+    val collectionConfigShowTags = CollectionConfig.fromCollectionJson(CollectionConfigJson.withDefaults(showTags = Option(true)))
+
+    "should resolve to None" in {
+      val curatedContent = CuratedContent.fromTrailAndContent(emptyContent, emptyTrailMetaData, collectionConfig)
+      curatedContent.kicker should be (None)
+    }
+
+    "should resolve to SectionKicker with config showSections true" in {
+      val curatedContent = CuratedContent.fromTrailAndContent(emptyContent, emptyTrailMetaData, collectionConfigShowSections)
+      curatedContent.kicker.value shouldBe a [SectionKicker]
+    }
+
+    "should resolve to TagKicker with config showTags true" in {
+      //This test requires content to have tags
+      val curatedContent = CuratedContent.fromTrailAndContent(emptyContent, emptyTrailMetaData, collectionConfigShowTags)
+      curatedContent.kicker.value shouldBe a [TagKicker]
+    }
+
+    "should resolve to SectionKicker for trailMetaData showKickerSection true" in {
+      val trailMetaDataShowKickerSection = TrailMetaData(Map("showKickerSection" -> JsBoolean(value = true)))
+      val curatedContent = CuratedContent.fromTrailAndContent(emptyContent, trailMetaDataShowKickerSection, collectionConfigShowTags)
+      curatedContent.kicker.value shouldBe a [SectionKicker]
+    }
+
+    "should resolve to SectionKicker for trailMetaData showKickerTag true" in {
+      val trailMetaDataShowKickerTag = TrailMetaData(Map("showKickerTag" -> JsBoolean(value = true)))
+      val curatedContent = CuratedContent.fromTrailAndContent(emptyContent, trailMetaDataShowKickerTag, collectionConfigShowTags)
+      curatedContent.kicker.value shouldBe a [TagKicker]
+    }
+  }
+
+  "SupportingCuratedContent itemKicker" - {
+    val emptyContent = Content(
+      "content-id", Some("section"), Some("Section Name"), None, "webTitle", "webUrl", "apiUrl",
+      fields = Some(Map("internalContentCode" -> "CODE", "headline" -> "Content headline", "href" -> "Content href", "trailText" -> "Content trailtext", "byline" -> "Content byline")),
+      List(Tag("id", "type", None, None, "", "", "")), None, Nil, None)
+    val emptyTrailMetaData = TrailMetaData(Map.empty)
+    val collectionConfig = CollectionConfig.fromCollectionJson(CollectionConfigJson.withDefaults())
+    val collectionConfigShowSections = CollectionConfig.fromCollectionJson(CollectionConfigJson.withDefaults(showSections = Option(true)))
+    val collectionConfigShowTags = CollectionConfig.fromCollectionJson(CollectionConfigJson.withDefaults(showTags = Option(true)))
+
+    "should resolve to None" in {
+      val supportingCuratedContent = SupportingCuratedContent.fromTrailAndContent(emptyContent, emptyTrailMetaData, collectionConfig)
+      supportingCuratedContent.kicker should be (None)
+    }
+
+    "should resolve to None with config showSections true" in {
+      val supportingCuratedContent = SupportingCuratedContent.fromTrailAndContent(emptyContent, emptyTrailMetaData, collectionConfigShowSections)
+      supportingCuratedContent.kicker should be (None)
+    }
+
+    "should resolve to None with config showTags true" in {
+      val supportingCuratedContent = SupportingCuratedContent.fromTrailAndContent(emptyContent, emptyTrailMetaData, collectionConfigShowTags)
+      supportingCuratedContent.kicker should be (None)
+    }
+
+    "should resolve to SectionKicker for trailMetaData showKickerSection true" in {
+      val trailMetaDataShowKickerSection = TrailMetaData(Map("showKickerSection" -> JsBoolean(value = true)))
+      val supportingCuratedContent = SupportingCuratedContent.fromTrailAndContent(emptyContent, trailMetaDataShowKickerSection, collectionConfigShowTags)
+      supportingCuratedContent.kicker.value shouldBe a [SectionKicker]
+    }
+
+    "should resolve to SectionKicker for trailMetaData showKickerTag true" in {
+      val trailMetaDataShowKickerTag = TrailMetaData(Map("showKickerTag" -> JsBoolean(value = true)))
+      val supportingCuratedContent = SupportingCuratedContent.fromTrailAndContent(emptyContent, trailMetaDataShowKickerTag, collectionConfigShowTags)
+      supportingCuratedContent.kicker.value shouldBe a [TagKicker]
     }
   }
 }
