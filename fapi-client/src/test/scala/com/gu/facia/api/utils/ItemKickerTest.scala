d45481da996973de6fc977b5a93fef272cca4894
Andrew Bulhak <andrew.bulhak@theguardian.com>
2015-03-05 14:40:42 +0000

    Added test for another possible form of HTML in a kicker

--- fapi-client/src/test/scala/com/gu/facia/api/utils/ItemKickerTest.scala
+++ fapi-client/src/test/scala/com/gu/facia/api/utils/ItemKickerTest.scala
@@ -78,6 +78,7 @@ class ItemKickerTest extends FreeSpec with ShouldMatchers with MockitoSugar with
 
     "should return nothing for free HTML kickers containing HTML" in {
       ItemKicker.kickerText(FreeHtmlKicker("<b>Something</b>")) shouldBe None
+      ItemKicker.kickerText(FreeHtmlKicker("Look at <a href=\"http://example.com\">this</a> link")) shouldBe None
       ItemKicker.kickerText(FreeHtmlKickerWithLink("<b>Something</b>", "http://www.theguardian.com/football")) shouldBe None
       ItemKicker.kickerText(FreeHtmlKickerWithLink("<a href=\"foo\">Something</b>", "http://www.theguardian.com/football")) shouldBe None
     }
