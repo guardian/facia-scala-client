19c91f49a460fe2b8d21aa40acc49ae6c6c2def0
Francis Carr <francis.carr@guardian.co.uk>
2015-04-21 11:34:20 +0100

    Add MediaType companion object

--- fapi-client/src/main/scala/com/gu/facia/api/utils/MediaType.scala
+++ fapi-client/src/main/scala/com/gu/facia/api/utils/MediaType.scala
@@ -1,5 +1,12 @@
 package com.gu.facia.api.utils
 
+import com.gu.facia.api.models.FaciaContent
+import FaciaContentImplicits._
+
+object MediaType {
+  def fromFaciaContent(faciaContent: FaciaContent): Option[MediaType] = faciaContent.mediaType
+}
+
 sealed trait MediaType
 
 case object Gallery extends MediaType
