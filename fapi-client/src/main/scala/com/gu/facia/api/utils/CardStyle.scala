1b3cf7dad385f6130dcc516e36ae527037de3caf
Sam Morris <mail@sammorrisdesign.co.uk>
2015-06-01 16:39:04 +0100

    Add new tag to specialReport CardStyle
    Yup

--- fapi-client/src/main/scala/com/gu/facia/api/utils/CardStyle.scala
+++ fapi-client/src/main/scala/com/gu/facia/api/utils/CardStyle.scala
@@ -23,7 +23,7 @@ object CardStyle {
     val href = trailMetaData.href.orElse(content.safeFields.get("href"))
     if (trailMetaData.snapType == Some("link") && href.exists(ExternalLinks.external)) {
       ExternalLink
-    } else if (content.tags.exists(_.id == "news/series/hsbc-files")) {
+    } else if (trail.tags.exists(_.id == "news/series/hsbc-files") || trail.tags.exists(_.id == "us-news/series/counted-us-police-killings")) {
       SpecialReport
     } else if (content.isLiveBlog) {
       if (content.isLive) {
