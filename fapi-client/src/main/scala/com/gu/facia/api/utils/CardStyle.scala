95f1931a550d62df19823ca1b82a7729c229bdfd
Oliver Joseph Ash <oliverjash@gmail.com>
2015-07-21 14:51:46 +0100

    Use SpecialReport for tag "australia-news/series/healthcare-in-detention"

--- fapi-client/src/main/scala/com/gu/facia/api/utils/CardStyle.scala
+++ fapi-client/src/main/scala/com/gu/facia/api/utils/CardStyle.scala
@@ -23,7 +23,9 @@ object CardStyle {
     val href = trailMetaData.href.orElse(content.safeFields.get("href"))
     if (trailMetaData.snapType == Some("link") && href.exists(ExternalLinks.external)) {
       ExternalLink
-    } else if (content.tags.exists(_.id == "news/series/hsbc-files") || content.tags.exists(_.id == "us-news/series/counted-us-police-killings")) {
+    } else if (content.tags.exists(_.id == "news/series/hsbc-files")
+      || content.tags.exists(_.id == "us-news/series/counted-us-police-killings")
+      || content.tags.exists(_.id == "australia-news/series/healthcare-in-detention")) {
       SpecialReport
     } else if (content.isLiveBlog) {
       if (content.isLive) {
