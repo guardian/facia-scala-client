822137005d89efe292a129bca44cafe481d563a1
Francis Carr <francis.carr@guardian.co.uk>
2015-10-21 14:39:37 +0100

    Add a MaxBatchSize into IdsSearchQueries makeBatches

--- fapi-client/src/main/scala/com/gu/facia/api/contentapi/IdsSearchQueries.scala
+++ fapi-client/src/main/scala/com/gu/facia/api/contentapi/IdsSearchQueries.scala
@@ -7,10 +7,14 @@ object IdsSearchQueries {
   type Url = String
 
   val MaxUrlSize = com.gu.contentapi.client.Limits.UrlSize
+  //Maximum size that can be requested from CAPI is 50.
+  val MaxBatchSize = 50
 
   def makeBatches(ids: Seq[Id])(urlFromIds: Seq[Id] => Url): Option[Seq[Seq[Id]]] = {
     def batchAndRemaining(ids: Seq[Id]): Option[(Seq[Id], Seq[Id])] =
-      ids.inits.find(urlFromIds(_).length <= MaxUrlSize) map { batch =>
+      ids.inits.find{ init =>
+        init.length <= MaxBatchSize && urlFromIds(init).length <= MaxUrlSize
+      } map { batch =>
         (batch, ids.drop(batch.length))
       }
 
