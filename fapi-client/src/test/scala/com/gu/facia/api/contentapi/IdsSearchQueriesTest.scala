1c8e6b1a6814132f46938f12764f5cf4c7252440
Francis Carr <francis.carr@guardian.co.uk>
2015-10-21 14:40:00 +0100

    Add tests for makeBatches

--- fapi-client/src/test/scala/com/gu/facia/api/contentapi/IdsSearchQueriesTest.scala
+++ fapi-client/src/test/scala/com/gu/facia/api/contentapi/IdsSearchQueriesTest.scala
@@ -46,4 +46,20 @@ class IdsSearchQueriesTest extends FreeSpec with ShouldMatchers {
       )))
     }
   }
+
+  "limit batches" - {
+    val fifty = List.fill(50)("abc").toSeq
+    val fiftyOne = fifty :+ "fiftyFirst"
+
+    "leave batches under or equal to 50" in {
+      IdsSearchQueries.makeBatches(fifty) {
+        _.mkString("")
+      } should be(Some(Seq(fifty)))
+    }
+
+    "should limit batches over 50" in {
+      IdsSearchQueries.makeBatches(fiftyOne){_.mkString("")} should be (Some(
+        Seq(fifty, Seq("fiftyFirst"))))
+    }
+  }
 }
