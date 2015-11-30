eb501599baf1458f468388e3ed15b68eb2a94db5
Fabio Crisci <fabio.crisci@guardian.co.uk>
2015-11-30 14:32:17 +0000

    update dependencies

--- project/dependencies.scala
+++ project/dependencies.scala
@@ -1,11 +1,11 @@
 import sbt._
 
 object Dependencies {
-  val awsSdk = "com.amazonaws" % "aws-java-sdk" % "1.7.9"
+  val awsSdk = "com.amazonaws" % "aws-java-sdk-s3" % "1.10.37"
   val commonsIo = "org.apache.commons" % "commons-io" % "1.3.2"
   val contentApi = "com.gu" %% "content-api-client" % "7.3"
-  val mockito = "org.mockito" % "mockito-all" % "1.10.8" % "test"
-  val playJson = "com.typesafe.play" %% "play-json" % "2.3.6"
-  val scalaTest = "org.scalatest" %% "scalatest" % "2.2.1" % "test"
+  val mockito = "org.mockito" % "mockito-all" % "1.10.19" % "test"
+  val playJson = "com.typesafe.play" %% "play-json" % "2.4.4"
+  val scalaTest = "org.scalatest" %% "scalatest" % "2.2.5" % "test"
   val specs2 = "org.specs2" %% "specs2" % "2.3.12" % "test"
 }
