86f33774e8dda23ba9c3e1f6c6bef2957b563ebf
Richard Nguyen <richard.nguyen@guardian.co.uk>
2016-01-06 13:41:57 +0000

    Update content api client to 7.12

--- project/dependencies.scala
+++ project/dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object Dependencies {
   val awsSdk = "com.amazonaws" % "aws-java-sdk-s3" % "1.10.37"
   val commonsIo = "org.apache.commons" % "commons-io" % "1.3.2"
-  val contentApi = "com.gu" %% "content-api-client" % "7.11"
+  val contentApi = "com.gu" %% "content-api-client" % "7.12"
   val mockito = "org.mockito" % "mockito-all" % "1.10.19" % "test"
   val playJson = "com.typesafe.play" %% "play-json" % "2.4.4"
   val scalaTest = "org.scalatest" %% "scalatest" % "2.2.5" % "test"
