aa89d684c8b60fc52c3331ed6d90b54ed555eb44
Fabio Crisci <fabio.crisci@guardian.co.uk>
2016-02-24 11:06:10 +0000

    downgrade test dependency
    because it conflicts with specs2

--- project/dependencies.scala
+++ project/dependencies.scala
@@ -6,6 +6,6 @@ object Dependencies {
   val contentApi = "com.gu" %% "content-api-client" % "7.27"
   val mockito = "org.mockito" % "mockito-all" % "1.10.19" % "test"
   val playJson = "com.typesafe.play" %% "play-json" % "2.4.6"
-  val scalaTest = "org.scalatest" %% "scalatest" % "2.2.6" % "test"
+  val scalaTest = "org.scalatest" %% "scalatest" % "2.2.5" % "test"
   val specs2 = "org.specs2" %% "specs2" % "3.7" % "test"
 }
