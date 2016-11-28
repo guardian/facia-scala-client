f62a42315d9dc79c226c131d72b0de897fe8bd9b
Philip McMahon <philip.mcmahon@guardian.co.uk>
2016-11-28 14:20:02 +0000

    Use correct bucket for images based on src image url.

--- facia-json/src/main/scala/com/gu/facia/client/MediaServiceClient.scala
+++ facia-json/src/main/scala/com/gu/facia/client/MediaServiceClient.scala
@@ -7,25 +7,28 @@ import org.joda.time.{DateTime, Period}
 import play.api.libs.json.{JsString}
 
 
-case class S3ClientAndBucket(s3Client: AmazonS3Client, bucketName: String)
+case class S3ClientAndBuckets(s3Client: AmazonS3Client, prodBucketName: String, testBucketName: String)
 
 
 object MediaServiceClient {
 
-  def generateThumbnailSecureUrl(imageSrc: String)(s3: S3ClientAndBucket) = {
+  def generateThumbnailSecureUrl(imageSrc: String)(s3: S3ClientAndBuckets) = {
     val idRegex = "[0-9a-z]{40}".r
     val mediaId = idRegex findFirstIn imageSrc
+
+    val bucket = if (imageSrc.contains("media-origin.test")) s3.testBucketName else s3.prodBucketName
 
     val key = mediaId.map(id => id.take(6).split("").map(c => s"$c/").mkString+id)
 
     val expiryDate = DateTime.now.withPeriodAdded(Period.minutes(1), 5).toDate
 
-    val signedUrl = key.map(s3.s3Client.generatePresignedUrl(s3.bucketName, _, expiryDate, HttpMethod.GET))
+
+    val signedUrl = key.map(s3.s3Client.generatePresignedUrl(bucket, _, expiryDate, HttpMethod.GET))
 
     signedUrl.map(_.toString).getOrElse("")
   }
 
-  def addThumbnailUrlsToTrailList(list: List[Trail])(s3: S3ClientAndBucket) = {
+  def addThumbnailUrlsToTrailList(list: List[Trail])(s3: S3ClientAndBuckets) = {
 
     val updatedImageSrcs = list.map(trail => trail.copy(meta = trail.meta.map{ meta =>
       meta.copy(json =
@@ -35,7 +38,7 @@ object MediaServiceClient {
     updatedImageSrcs
   }
 
-  def addThumbnailsToCollection(collection: CollectionJson)(s3: S3ClientAndBucket) = {
+  def addThumbnailsToCollection(collection: CollectionJson)(s3: S3ClientAndBuckets) = {
     collection.copy(
       live = addThumbnailUrlsToTrailList(collection.live)(s3),
       draft = collection.draft.map(addThumbnailUrlsToTrailList(_)(s3))
