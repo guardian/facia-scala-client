86e0dac2dcc3fa328089d97079198efb254b543c
Gideon Goldberg <gidsgoldberg@gmail.com>
2016-12-05 13:51:16 +0000

    update capi client

--- project/dependencies.scala
+++ project/dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object Dependencies {
   val awsSdk = "com.amazonaws" % "aws-java-sdk-s3" % "1.11.7"
   val commonsIo = "org.apache.commons" % "commons-io" % "1.3.2"
-  val contentApi = "com.gu" %% "content-api-client" % "10.1"
+  val contentApi = "com.gu" %% "content-api-client" % "10.12"
   val mockito = "org.mockito" % "mockito-all" % "1.10.19" % "test"
   val playJson = "com.typesafe.play" %% "play-json" % "2.4.6"
   val playJson25 = "com.typesafe.play" %% "play-json" % "2.5.4"
