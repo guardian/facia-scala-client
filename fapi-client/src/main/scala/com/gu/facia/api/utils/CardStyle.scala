611afdc575685e82a5d9fe082dba06b4b6ed58f4
Maria Livia Chiorean <maria-livia.chiorean@guardian.co.uk>
2017-05-12 14:54:04 +0100

    Revert "Label paid content accordingly"

--- fapi-client/src/main/scala/com/gu/facia/api/utils/CardStyle.scala
+++ fapi-client/src/main/scala/com/gu/facia/api/utils/CardStyle.scala
@@ -20,7 +20,6 @@ object CardStyle {
   val letters = "letters"
   val external = "external"
   val news = "news"
-  val paid = "paid"
 
   private val salt = "a-public-salt3W#ywHav!p+?r+W2$E6="
   private val digest = MessageDigest.getInstance("MD5")
@@ -57,8 +56,6 @@ object CardStyle {
       || hashedTagIds.contains("7037b49de72275eb72b73a111da31849")      // australia-news/series/healthcare-in-detention
       || hashedTagIds.contains("efb4e63b9a3a926314724b45764a5a5a") ) {  // society/series/this-is-the-nhs
       SpecialReport
-    } else if (content.isPaid) {
-      Paid
     } else if (content.isLiveBlog) {
       if (content.isLive) {
         LiveBlog
@@ -133,10 +130,6 @@ case object Letters extends CardStyle {
 
 case object ExternalLink extends CardStyle {
   val toneString = CardStyle.external
-}
-
-case object Paid extends CardStyle {
-  val toneString = CardStyle.paid
 }
 
 case object DefaultCardstyle extends CardStyle {
