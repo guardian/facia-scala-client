763d05a63c1df585826c96e7ba0734a628236f9b
Alexandre Dufournet <alex.dufournet@theguardian.com>
2018-04-11 15:12:46 +0100

    bump capi client

--- project/dependencies.scala
+++ project/dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object Dependencies {
   val awsSdk = "com.amazonaws" % "aws-java-sdk-s3" % "1.11.154"
   val commonsIo = "org.apache.commons" % "commons-io" % "1.3.2"
-  val contentApi = "com.gu" %% "content-api-client" % "11.33"
+  val contentApi = "com.gu" %% "content-api-client" % "11.55"
   val mockito = "org.mockito" % "mockito-all" % "1.10.19" % "test"
   val playJson24 = "com.typesafe.play" %% "play-json" % "2.4.6"
   val playJson25 = "com.typesafe.play" %% "play-json" % "2.5.4"
