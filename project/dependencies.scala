96129cafea75d064c1617794ca8a14ca8ff8b6a2
Roberto Tyley <roberto.tyley@gmail.com>
2023-08-05 22:36:33 +0100

    Dep updates

--- project/dependencies.scala
+++ project/dependencies.scala
@@ -9,7 +9,7 @@ object Dependencies {
   val contentApiDefault = "com.gu" %% "content-api-client-default" % capiVersion % Test
   val mockito = "org.mockito" % "mockito-all" % "1.10.19" % Test
   val scalaTestMockito = "org.scalatestplus" %% "mockito-4-11" % "3.2.16.0" % Test
-  val scalaTest = "org.scalatest" %% "scalatest" % "3.2.15" % Test
-  val scalaLogging = "com.typesafe.scala-logging" %% "scala-logging" % "3.9.2"
-  val commercialShared = "com.gu" %% "commercial-shared" % "6.1.6"
+  val scalaTest = "org.scalatest" %% "scalatest" % "3.2.16" % Test
+  val scalaLogging = "com.typesafe.scala-logging" %% "scala-logging" % "3.9.5"
+  val commercialShared = "com.gu" %% "commercial-shared" % "6.1.7"
 }
