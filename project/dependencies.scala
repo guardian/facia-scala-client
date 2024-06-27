8cb5288d6ae5a7a9098e669d03ce739912726617
David Furey <david.furey@guardian.co.uk>
2024-06-27 10:21:50 +0100

    Update play-json to 3.0.2

--- project/dependencies.scala
+++ project/dependencies.scala
@@ -28,6 +28,6 @@ object Dependencies {
     val V27 = PlayJsonVersion("27", "com.typesafe.play", "2.7.4")
     val V28 = PlayJsonVersion("28", "com.typesafe.play", "2.8.2")
     val V29 = PlayJsonVersion("29", "com.typesafe.play", "2.10.4", supportsScala3 = true)
-    val V30 = PlayJsonVersion("30", "org.playframework", "3.0.1", supportsScala3 = true)
+    val V30 = PlayJsonVersion("30", "org.playframework", "3.0.2", supportsScala3 = true)
   }
 }
