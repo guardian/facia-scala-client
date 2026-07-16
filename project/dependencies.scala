06be11faecd77304bd976b2f200a8c958f1888bd
Georges Lebreton <georges.lebreton@guardian.co.uk>
2026-07-16 16:18:47 +0100

    use latest version with temp

--- project/dependencies.scala
+++ project/dependencies.scala
@@ -1,7 +1,7 @@
 import sbt._
 
 object Dependencies {
-  val capiVersion = "45.0.0-PREVIEW.add-football-competition-atom.2026-07-10T1509.202d1e09"
+  val capiVersion = "45.0.0-PREVIEW.add-football-competition-atom.2026-07-16T1458.7529b092"
   val eTagCachingVersion = "7.0.0"
 
   val eTagCachingS3Base = "com.gu.etag-caching" %% "aws-s3-base" % eTagCachingVersion
