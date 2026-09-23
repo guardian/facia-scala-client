8179e644e1986c77bb7b893e3d73ac3cc352ced5
Anna Beddow <anna.beddow@gmail.com>
2026-09-23 11:07:21 +0100

    Bump CAPI version to 50.0.0-PREVIEW.abadd-search-for-active-tests.2026-09-23T0803.f7763ae4

--- project/dependencies.scala
+++ project/dependencies.scala
@@ -1,7 +1,7 @@
 import sbt._
 
 object Dependencies {
-  val capiVersion = "49.1.1"
+  val capiVersion = "50.0.0-PREVIEW.abadd-search-for-active-tests.2026-09-23T0803.f7763ae4"
   val eTagCachingVersion = "18.0.0"
 
   val eTagCachingS3Base =
